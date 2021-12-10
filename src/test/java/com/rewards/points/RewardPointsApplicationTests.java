45cff67e7964d554db2bf86b6ed76e852a4faa80
rupa.boyina@gmail.com <rupa.boyina@gmail.com>
2021-12-10 12:04:26 -0600

    Added test cases

--- src/test/java/com/rewards/points/RewardPointsApplicationTests.java
+++ src/test/java/com/rewards/points/RewardPointsApplicationTests.java
@@ -1,13 +1,68 @@
 package com.rewards.points;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
-class RewardPointsApplicationTests {
+@AutoConfigureMockMvc
+public class RewardPointsApplicationTests {
 
+	@Autowired
+	protected MockMvc mvc;
+	
+	private String fileName = "/sample.json";
+	
 	@Test
-	void contextLoads() {
+	void testRewardsSummaryRes() throws Exception {
+
+		JSONArray data = (JSONArray) (new JSONParser()).parse(new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(fileName))));
+		MvcResult result = this.mvc.perform(MockMvcRequestBuilders.post("/rewards/getRewardsInfo")
+				.content(data.toJSONString())
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String res = result.getResponse().getContentAsString();
+		assertNotNull(res);
+	}
+	
+	@Test
+	void testTotalRewards() throws Exception {
+		JSONArray data = (JSONArray) (new JSONParser()).parse(new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(fileName))));
+		this.mvc.perform(MockMvcRequestBuilders.post("/rewards/getRewardsInfo")
+				.content(data.toJSONString())
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].totalRewards").value("762"))
+				.andReturn();
+	}
+	
+	@Test
+	void testBadRequest() throws Exception {
+
+		this.mvc.perform(MockMvcRequestBuilders.post("/rewards/getRewardsInfo")
+				.content("")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().is4xxClientError())
+				.andReturn();
 	}
 
 }
